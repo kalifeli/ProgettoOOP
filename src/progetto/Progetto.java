e679e3eb9c8379b4e80cf50b30f91e0202891a51
Alessandro Feliziani <felizianiale34@gmail.com>
2023-01-10 10:32:38 +0100

    aggiunta delle istruzioni per la lettura da stdin dei dati per popolare gli oggetti del varie classi, problemi nella stampa di getRegaliTotali() e getRegaliDistinti(). Non so ancora se il problema risiede nella scrittura errata dei metodi o nel codice che legge in modo errato i dati

--- src/progetto/Progetto.java
+++ src/progetto/Progetto.java
@@ -1,101 +1,97 @@
 package progetto;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Progetto {
     public static void main(String[] args) {
-        /*
-                r1 10 arte
-                r2 5 elettronica
-                r3 8 arte
-                r4 6 sport
-                r5 12 elettronica
-        */
 
-        // Creazione delle istanze dei regali
-        Regalo r1 = new Regalo("r1",10,"arte");
-        Regalo r2 = new Regalo("r2",5,"elettronica");
-        Regalo r3 = new Regalo("r3",8,"arte");
-        Regalo r4 = new Regalo("r4",6,"sport");
-        Regalo r5 = new Regalo("r5",12,"elettronica");
+        Scanner scanner = new Scanner(System.in);
 
-        /*
-            p1 4
-            p2 1
-         */
+        Settore s1 = new Settore(); // genera una lista di pianeti
 
-        // creazione delle istanze dei pianeti
-        Pianeta p1 = new Pianeta("p1"); // ha 4 città
-        Pianeta p2 = new Pianeta("p2"); // ha 1 città
+        String temp = scanner.nextLine();
+        String[] campi = temp.split(" ");
+        s1.setDeposito(Integer.parseInt(campi[0]));
+        int numeroPianeti = Integer.parseInt(campi[1]);
+        int numeroTotaleRegali = Integer.parseInt(campi[2]);
+        //creo una lista dei regali
+        ArrayList<Regalo> listaRegali = new ArrayList<>();
+        //aggiungo i regali letti da stdin nella listaRegali
+        for(int i = 0; i < numeroTotaleRegali; ++i)
+        {
+            temp = scanner.nextLine();
+            campi = temp.split(" ");
+            Regalo regaloTemp = new Regalo(campi[0],Double.parseDouble(campi[1]),campi[2]);
+            listaRegali.add(regaloTemp);
+        }
+        // genero la lista dei pianeti con i pianeti letti in input dal file
+        int numeroCittaTotali = 0;
+        for(int i = 0; i < numeroPianeti; ++i)
+        {
+            temp = scanner.nextLine();
+            campi = temp.split(" ");
+            Pianeta pianetaTemp = new Pianeta(campi[0],Integer.parseInt(campi[1]));
+            s1.addPianeta(pianetaTemp);
+            numeroCittaTotali += pianetaTemp.getNumeroCitta();
+        }
 
+        // istanzio le citta e aggiungerle nei rispettivi pianeti di appartenenza
+        for(int i = 0; i < numeroCittaTotali; ++i)
+        {
+            temp = scanner.nextLine();
+            campi = temp.split(" ");
+            Citta cittaTemp = new Citta(campi[0],Integer.parseInt(campi[2]));
+            //se il file presenta i collegamenti con altre citta
+            if(!campi[3].equals("-")){
+                //creo un array di String che contiene campi di questo tipo x#y
+                String[] collegamenti = campi[3].split(",");
 
+                //su tutti i campi di collegamenti
+                for(int j = 0; j < collegamenti.length; ++j){
+                    //creo un aray di String che mi rappresenti nel campo[0] == citta2, campo[1] == costo
+                    // datiCollegamenti viene ad ogni iterazione del ciclo for
+                    String[] datiCollegamenti = collegamenti[j].split("#");
+                    // istanzio un collegamento temporaneo che mi permette di tener traccia ogni volta dei collegamenti che creo
+                    Collegamento collegamentoTemp = new Collegamento(campi[0],datiCollegamenti[0],Integer.parseInt(datiCollegamenti[1]));
+                    //aggiungo ogni volta il collegamento all'interno della listaCollegamenti della città
+                    cittaTemp.addCollegamento(collegamentoTemp);
+                }
+            }
 
-        /*
-        c1 p1 1 c2#2,c3#8
-        c2 p1 2 c1#2,c3#4,c4#12
-        c3 p1 1 c1#8,c2#4,c4#3
-        c4 p1 4 c2#12,c3#3
-        c5 p5 3 -
-         */
-        // Creazione delle istanze delle citta e assegnazione ai rispettivi pianeti
-        Citta c1 = new Citta("c1",1);
-        p1.getListaCitta().add(c1);
-        Citta c2 = new Citta("c2",2);
-        p1.getListaCitta().add(c2);
-        Citta c3 = new Citta("c3",1);
-        p1.getListaCitta().add(c3);
-        Citta c4 = new Citta("c4",4);
-        p1.getListaCitta().add(c4);
-        Citta c5 = new Citta("c5",3);
-        p2.getListaCitta().add(c5);
+            for(Pianeta pianeta : s1.getListaPianeti())
+            {
+                if(campi[1].equals(pianeta.getNomePianeta()))
+                {
+                    pianeta.addCitta(cittaTemp);
+                }
+            }
+        }
 
-        /*
-        c1 : r1,r2,r3
-        c2 : r2,r4
-        c3 : r1,r3,r4
-        c4 : r1,r2
-        c5 : r2,r4
-         */
-        // Aggiunta dei regali alle citta
-        c1.addRegalo(r1);
-        c1.addRegalo(r2);
-        c1.addRegalo(r3);
+        for(int i = 0; i < numeroCittaTotali; ++i)
+        {
+            temp = scanner.nextLine();
+            campi = temp.split(" : ");
+            for(Pianeta pianeta : s1.getListaPianeti())
+                for(Citta citta : pianeta.getListaCitta())
+                {
+                    if(citta.getNomeCitta().equals(campi[0])){
+                        String[] regaliTemp = campi[1].split(",");
+                        for(int j = 0; j < regaliTemp.length; ++j)
+                        {
+                            for(Regalo regalo : listaRegali){
+                                if(regaliTemp[0].equals(regalo.getNomeRegalo())) {
+                                    citta.addRegalo(regalo);
+                                }
+                            }
 
+                        }
 
-        c2.addRegalo(r2);
-        c2.addRegalo(r4);
+                    }
 
-        c3.addRegalo(r1);
-        c3.addRegalo(r3);
-        c3.addRegalo(r4);
+                }
 
-        c4.addRegalo(r1);
-        c4.addRegalo(r2);
-
-        c5.addRegalo(r2);
-        c5.addRegalo(r4);
-
-        Settore s1 = new Settore(100);
-        s1.addPianeta(p1);
-        s1.addPianeta(p2);
-
-        //COLLEGAMENTI
-        Collegamento collegamento1 = new Collegamento(c1,c2,2);
-        c1.addCollegamento(collegamento1);
-
-        Collegamento collegamento2 = new Collegamento(c1,c3,8);
-        c1.addCollegamento(collegamento2);
-
-        c2.addCollegamento(new Collegamento(c2,c1,2));
-        c2.addCollegamento(new Collegamento(c2,c3,4));
-        c2.addCollegamento(new Collegamento(c2,c4,12));
-
-        c3.addCollegamento(new Collegamento(c3,c1,8));
-        c3.addCollegamento(new Collegamento(c3,c2,4));
-        c3.addCollegamento(new Collegamento(c3,c4,3));
-
-        c4.addCollegamento(new Collegamento(c4,c2,12));
-        c4.addCollegamento(new Collegamento(c4,c3,3));
-
-        //TASK 1 (sono riportati i risultati affianco ad ogni stampa)
+        }
         System.out.println(s1.getRegaliTotali() + " " + s1.getRegaliDistinti()); // 12 4
         s1.getRegaliTotaliPerTipologia(); // 5 4 3
         System.out.println(s1.getPianetaMostRegali()); // p1
@@ -104,37 +100,13 @@ public class Progetto {
         System.out.println(s1.getCittadinaMostCase());
         System.out.println(s1.getCittaMostCollegamenti());// c2
 
-        //TASK 2 (sono riportati i risultati affianco ad ogni stampa)
-        if(s1.task2_1(1,10)) // yes
+        if(s1.task2_1(1,10))
             System.out.println("YES");
-        else
-            System.out.println("NO");
-
-        if(s1.task2_2()) // yes
+        if (s1.task2_2())
             System.out.println("YES");
-        else
-            System.out.println("NO");
-
-
-
-
-
-
-
-        /*
-        c1 p1 1 c2#2,c3#8 -> 10
-        c2 p1 2 c1#2,c3#4,c4#12 -> 18
-        c3 p1 1 c1#8,c2#4,c4#3 -> 15
-        c4 p1 4 c2#12,c3#3 -> 15
-        c5 p5 3 -
-         */
-
-
-
-
-
-
-
+        System.out.println();
+        if(s1.task2_4())
+            System.out.println("YES");
 
     }
 }
