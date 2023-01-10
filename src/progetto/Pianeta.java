ca7f37a79b2f774352dca5229d77bf7c7fb2d030
Alessandro Feliziani <felizianiale34@gmail.com>
2023-01-10 10:27:44 +0100

    Aggiunta di un attributo che indica il numero di citta nel Pianeta e che viene inizializzato nel costruttore che è quindi stato modificato. Aggiunta di un metodo addCitta che aggiunge una città nella lista delle città del Pianeta

--- src/progetto/Pianeta.java
+++ src/progetto/Pianeta.java
@@ -3,25 +3,26 @@ package progetto;
 import java.util.ArrayList;
 public class Pianeta {
     private String nomePianeta;
-    private int regaliTotali; // regali del pianeta
+    private int numeroCitta;
     private ArrayList<Citta> listaCitta;
 
     /**
      * Costruttore della classe Pianeta che è descritto univocamente da un nome e da il numero dei regali totali
      * @param nomePianeta nome univoco per il pianeta
      */
-    public Pianeta(String nomePianeta){
+    public Pianeta(String nomePianeta,int numeroCitta){
         this.nomePianeta = nomePianeta;
+        this.numeroCitta = numeroCitta;
         this.listaCitta = new ArrayList<>();
     }
     public String getNomePianeta(){
         return nomePianeta;
     }
-    public int getRegaliTotali(){
-        return regaliTotali;
-    }
+    public int getNumeroCitta() {return numeroCitta;}
     public ArrayList<Citta> getListaCitta(){
         return listaCitta;
     }
+
+    public void addCitta(Citta citta){listaCitta.add(citta);}
 
 }
