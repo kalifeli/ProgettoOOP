3fd6e19dfa087ae94932f9dc5d844e89db4502df
Alessandro Feliziani <felizianiale34@gmail.com>
2023-01-11 13:02:44 +0100

    aggiunta per tutti gli esercizi da 3 a 7 della task1 un controllo che restituisce in caso di presenza di piu nomi validi, il primo in ordine lessicografico crescente

--- src/progetto/Settore.java
+++ src/progetto/Settore.java
@@ -44,7 +44,7 @@ public class Settore {
             }
         }
         return numeroRegaliTotali;
-    };
+    }
 
     /**
      * Task 1) 1.Metodo che utilizza un istanza di HashMap che calcola il numero di regali distinti utilizzando semplicemente la size della HasMap
@@ -71,21 +71,31 @@ public class Settore {
     /**
      * Task 1) 2. Metodo che stampa il numero dei regali, di un settore, di ogni tipologia
      */
-    public void getRegaliTotaliPerTipologia(){
-        HashMap<String,Integer> contatoreTipologie = new HashMap<>();
-        for(Pianeta pianeta : getListaPianeti())
-            for(Citta c : pianeta.getListaCitta())
-                for(Regalo regalo : c.getListaRegali())
-                {
-                    String tipo = regalo.getTipoRegalo();
-                    int contatore = contatoreTipologie.getOrDefault(tipo,0);
-                    contatoreTipologie.put(tipo, contatore + 1);
-                }
-        int regaliDiArte = contatoreTipologie.getOrDefault("arte", 0);
-        int regaliDiElettronica = contatoreTipologie.getOrDefault("elettronica", 0);
-        int regaliDiSport = contatoreTipologie.getOrDefault("sport",0);
+    public void task1_2(){
+        int regaliArte = 0;
+        int regaliElettronica = 0;
+        int regaliSport = 0;
 
-        System.out.println(regaliDiArte + " " + regaliDiElettronica + " " + regaliDiSport);
+        for(Pianeta pianeta : getListaPianeti()){
+            for(Citta citta : pianeta.getListaCitta())
+            {
+                for(Regalo regalo : citta.getListaRegali()) {
+                    switch (regalo.getTipoRegalo())
+                    {
+                        case "arte":
+                            regaliArte++;
+                            break;
+                        case "elettronica":
+                            regaliElettronica++;
+                            break;
+                        case "sport":
+                            regaliSport++;
+                            break;
+                    }
+                }
+            }
+        }
+        System.out.println(regaliArte + " " + regaliElettronica + " " + regaliSport);
     }
 
 
@@ -94,6 +104,8 @@ public class Settore {
      * @return nome del pianeta con il numero maggiore di regali richiesti
      */
     public String getPianetaMostRegali(){
+        // una lista in cui introduco nomi di pianeta che hanno lo stesso numero di regali massimi
+        ArrayList<String> nomiValidi = new ArrayList<>();
         int maxRegali = 0;
         String pianetaMostRegali = "";
 
@@ -106,19 +118,35 @@ public class Settore {
             if(numeroRegali > maxRegali){
                 maxRegali = numeroRegali;
                 pianetaMostRegali = pianeta.getNomePianeta();
+                nomiValidi.clear();
+                nomiValidi.add(pianetaMostRegali);
+            } else if (numeroRegali == maxRegali) {
+                nomiValidi.add(pianeta.getNomePianeta());
             }
         }
-        return pianetaMostRegali;
+        if(nomiValidi.size() > 0){
+            String primoPerOrdine = nomiValidi.get(0);
+            for(int i = 1; i < nomiValidi.size(); ++i){
+                if(nomiValidi.get(i).compareTo(primoPerOrdine) < 0)
+                {
+                    primoPerOrdine = nomiValidi.get(i);
+                }
+            }
+            return primoPerOrdine;
+        }
+        return "";
     }
     /**
      * Task 1) 4.Metodo che restituisce il nome del pianeta che presenta costo maggiore di regali richiesti
      */
     public String getPianetaMostCosto() {
+        ArrayList<String> nomiValidi = new ArrayList<>();
         String pianetaMostCosto = "";
         // inizializzo la variabile che conterrà il costo massimo dei regali richiesti in un pianeta
         double costoMax = 0;
+
         for(Pianeta pianeta : getListaPianeti())
-        {
+        {  // calcolo costoMax di ogni pianeta
             double costo = 0;
             for(Citta citta : pianeta.getListaCitta())
             {
@@ -131,17 +159,30 @@ public class Settore {
             {
                 costoMax = costo;
                 pianetaMostCosto = pianeta.getNomePianeta();
+                nomiValidi.clear();
+                nomiValidi.add(pianetaMostCosto);
+            }else if(costo == costoMax){
+                nomiValidi.add(pianeta.getNomePianeta());
             }
         }
-        return pianetaMostCosto;
+        if(nomiValidi.size() > 0){
+            String primoInOrdine = nomiValidi.get(0);
+            for(int i = 1; i < nomiValidi.size(); ++i){
+                if(nomiValidi.get(i).compareTo(primoInOrdine) < 0)
+                    primoInOrdine = nomiValidi.get(i);
+            }
+            return primoInOrdine;
+        }
+        return "";
 
     }
 
     /**
      * Task 1) 5.Metodo che restituisce il nome della città col numero massimo di regali richiesti
-     * @return
+     * @return Stringa rappresentante al citta con il numero maggiore di regali richiesti
      */
     public String getCittaMostRegali(){
+        ArrayList<String> nomiValidi = new ArrayList<>();
         String cittaMostRegali = "";
         int maxRegali = 0;
         for (Pianeta pianeta : getListaPianeti()){
@@ -151,60 +192,90 @@ public class Settore {
                 {
                     maxRegali = regaliRichiesti;
                     cittaMostRegali = citta.getNomeCitta();
+                    nomiValidi.clear(); // pulire la mia lista di nomi validi visto che ho trovato una nuova citta con numero regali massimo
+                    nomiValidi.add(cittaMostRegali); // vado ad aggiungerla ai nomi candidati per essere la citta con più regali richiesti
+                }else if(regaliRichiesti == maxRegali){
+                    nomiValidi.add(citta.getNomeCitta());
                 }
             }
         }
-        return cittaMostRegali;
+        if(nomiValidi.size() > 0)
+        {
+            String primoNomeInOrdine = nomiValidi.get(0); // inizializzo la variabile string con il primo nome della lista nomiValidi
+            for(int i = 1; i < nomiValidi.size(); ++i){
+                if(nomiValidi.get(i).compareTo(primoNomeInOrdine) < 0){
+                    primoNomeInOrdine = nomiValidi.get(i);
+                }
+            }
+            return primoNomeInOrdine;
+        }
+        return "";
     }
 
     /**
      * Task 1) 6.Metodo che restituisce il nome della cittadina contenente più case
-     * @return
+     * @return una Stringa rappresentante il nome della cittadina con il numero maggiore di case
      */
     public String getCittadinaMostCase(){
-        String cittadinaMostcase = "";
+        ArrayList<String> nomiValidi = new ArrayList<>();
+        String cittadinaMostcase;
         int maxCase = 0;
         for (Pianeta pianeta : getListaPianeti()){
             for (Citta citta : pianeta.getListaCitta()){
                 if (!citta.isPaesino() && citta.getNumeroCase() > maxCase) {
                     maxCase = citta.getNumeroCase();
                     cittadinaMostcase = citta.getNomeCitta();
+                    nomiValidi.clear();
+                    nomiValidi.add(cittadinaMostcase);
+                } else if (!citta.isPaesino() && citta.getNumeroCase() == maxCase) {
+                    nomiValidi.add(citta.getNomeCitta());
                 }
             }
         }
-        return cittadinaMostcase;
+        if(nomiValidi.size() > 0){
+            String primaPerOrdine = nomiValidi.get(0);
+            for(int i = 1; i < nomiValidi.size(); ++i){
+                if(nomiValidi.get(i).compareTo(primaPerOrdine) < 0)
+                    primaPerOrdine = nomiValidi.get(i);
+            }
+            return primaPerOrdine;
+        }
+        return "";
     }
 
     /**
-     * Metodo che restituisce il nome della cittadina con più collegamenti
+     * Metodo che restituisce il nome della citta con più collegamenti
      * @return citta che ha più collegamenti con altre città
      */
 
     public String getCittaMostCollegamenti(){
+        ArrayList<String> nomiValidi = new ArrayList<>();
         int maxCollegamenti = 0;
-        String cittaMostCollegamenti = "";
+        String cittaMostCollegamenti;
         for (Pianeta pianeta : getListaPianeti()){
             for (Citta citta : pianeta.getListaCitta()){
                 int contatoreCollegamento = citta.getListaCollegamenti().size();
                 if (contatoreCollegamento > maxCollegamenti){
                     maxCollegamenti = contatoreCollegamento;
                     cittaMostCollegamenti = citta.getNomeCitta();
+                    nomiValidi.clear();
+                    nomiValidi.add(cittaMostCollegamenti);
+                } else if (contatoreCollegamento == maxCollegamenti) {
+                    nomiValidi.add(citta.getNomeCitta());
                 }
             }
         }
-        return cittaMostCollegamenti;
+        if(nomiValidi.size() > 0){
+            String primaPerOrdine = nomiValidi.get(0);
+            for(int i = 1; i < nomiValidi.size(); ++i){
+                if(nomiValidi.get(i).compareTo(primaPerOrdine) < 0)
+                    primaPerOrdine = nomiValidi.get(i);
+            }
+            return primaPerOrdine;
+        }
+        return "";
     }
     //TASK 2
-
-    /**
-     * Stampa Yes se la condizione è soddisfatta altrimenti NO
-     */
-    public void stampaTask2(){
-        if(true)
-            System.out.println("YES");
-        else
-            System.out.println("NO");
-    }
 
     /**
      * Metodo booleano che restituisce TRUE se il numero totale di regali
@@ -259,9 +330,16 @@ public class Settore {
     }
 
     /**
+     * Metodo che torna true se per ogni pianeta, non vi sono tre città collegate tra loro in cui sono stati richiesti lo stesso
+     * numero di regali di tipo arte ed elettronica
+     * @return
+     */
+
+    /**
      * Metodo booleano che restituisce true se per ogni pianeta non vi sono due città in cui non è stato richiesto almeno un regalo dello
      * stesso tipo
-     * @return
+     * @return true se per ogni pianeta non vi sono due città in cui non è stato richiesto almeno un regalo dello
+     * stesso tipo, altrimenti false.
      */
     public boolean task2_4() {
         for (Pianeta p : getListaPianeti()) {
